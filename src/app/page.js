528929e909e730549570d89bf13fd179ce485b99
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2025-05-24 03:08:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Navigation from '@/components/Navigation';
+import Navigation from './components/ui/Navigation';
 
 export default function HomePage() {
   return (
